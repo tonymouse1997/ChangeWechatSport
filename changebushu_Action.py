b52178e254c64b78c8090db4269afd5e77146e95
tonymouse1997 <33424320+tonymouse1997@users.noreply.github.com>
2023-05-22 20:04:11 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -109,7 +109,6 @@ if __name__ == "__main__":
         user = eval(os.environ['USER_PHONE'])[i]
         password = eval(os.environ['USER_PWD'])[i]
         print(user, password)
-        step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
-        # step = os.environ['STEP']
-        # step = str(randint(10123, 12302)) 
+        # step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
+        # step = '12345'
         main()
