4d84c9851fff24122ebe66968b7f8c295250d2d9
tonymouse1997 <33424320+tonymouse1997@users.noreply.github.com>
2023-05-22 17:22:52 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -104,9 +104,11 @@ def main_handler(event, context):
  
  
 if __name__ == "__main__":
-    user = os.environ['USER_PHONE']
-    password = os.environ['USER_PWD']
-    step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
-    # step = os.environ['STEP']
-    # step = str(randint(10123, 12302)) 
-    main()
+    for i in range(len(eval(os.environ['USER_PHONE']))):
+        
+        user = eval(os.environ['USER_PHONE'])[i]
+        password = eval(os.environ['USER_PWD'])[i]
+        step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
+        # step = os.environ['STEP']
+        # step = str(randint(10123, 12302)) 
+        main()
