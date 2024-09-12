1d1018ae247777b72a333ec83c760107a9bdb961
tonymouse1997 <33424320+tonymouse1997@users.noreply.github.com>
2024-09-12 10:18:20 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -86,9 +86,11 @@ def main():
  
 
 def get_time():
-    url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
-    response = requests.get(url, headers=headers).json()
-    t = response['data']['t']
+    # url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
+    # response = requests.get(url, headers=headers).json()
+    # t = response['data']['t']
+    import time
+    t = int(time.time())
     return t
  
 
